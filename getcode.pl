c9092b0f2f171e1f47cc3a2d4867a07f329a8ec8
Edoardo Mantovani <Baseband@cpan.org>
2020-11-26 08:37:52 +0100

    Delete getcode.pl

--- getcode.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-print "Content-Type: text/html \n\n";
-# This is a simple script with comments
-# that explain what the code does.
-# These comments do not affect the way
-# the script works.
-print "Welcome to ACME AUTO!"; # You
-# can even put comments on the same line
-# as executable code.
